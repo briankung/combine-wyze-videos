75ca344e27d333f81a5cf4dde504cc8d0d342862
Brian Kung <github@briankung.xyz>
2023-04-16 15:00:48 -0500

    Update to only check the top level folders

--- combine-videos.sh
+++ combine-videos.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
-for folder in ./2023*; do
+for folder in $(fd . -td -L1); do
     # New process to avoid having to navigate relative paths
     (
         cd "$folder";
-        echo "🗂️🗂️🗂️🗂️🗂️ $folder 🗂️🗂️🗂️🗂️🗂️"
-        ls -d **/*.mp4 | rg '(.+)' -r 'file $1' > files.txt
-        cat files.txt
-        ffmpeg -f concat -safe 0 -i files.txt -c copy ./combined.mp4;
+        echo "🗂️🗂️🗂️🗂️🗂️ $folder 🗂️🗂️🗂️🗂️🗂️";
+        ls -d **/*.mp4 | rg '(.+)' -r 'file $1' > files.txt;
+        # cat files.txt;
+        ffmpeg -f concat -safe 0 -i files.txt -c copy ./$folder-combined.mp4;
+        rm files.txt;
     )
 done
