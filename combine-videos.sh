492c44f07a6913599e17353857acc6e6297227dc
Brian Kung <github@briankung.xyz>
2023-05-29 21:59:12 -0500

    Fixes

--- combine-videos.sh
+++ combine-videos.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
-for folder in $(fd . -td -L1); do
+# This is supposed to be on the same level as the individual days in the wyze
+# cam `record` folder.
+
+for folder in $(fd . -td -d1); do
     # New process to avoid having to navigate relative paths
     (
+        folder=$(basename "$folder");
         cd "$folder";
-        echo "🗂️🗂️🗂️🗂️🗂️ $folder 🗂️🗂️🗂️🗂️🗂️";
+        echo "🗂️🗂️🗂️🗂️🗂️   $folder 🗂️🗂️🗂️🗂️🗂️";
         ls -d **/*.mp4 | rg '(.+)' -r 'file $1' > files.txt;
         # cat files.txt;
-        ffmpeg -f concat -safe 0 -i files.txt -c copy ./$folder-combined.mp4;
+        ffmpeg -f concat -safe 0 -i files.txt -c copy ../$folder-combined.mp4;
         rm files.txt;
     )
 done
